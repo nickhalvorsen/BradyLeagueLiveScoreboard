87c18caf2c42c93f71dcba09350368a590b8d617
Nick Halvorsen <nick@hugemail.win>
2023-11-13 21:55:17 -0500

    convert index to ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import { store } from "./store/store";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
